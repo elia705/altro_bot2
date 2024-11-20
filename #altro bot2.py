0c9be588361ce6076c6f8dbfa75289c20f191483
elia705 <elia.costantini.11@gmail.com>
2024-11-20 18:27:33 +0100

    Add files via upload

---
+++ #altro bot2.py
@@ -0,0 +1,27 @@
+#altro bot
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hai fatto l\'accesso come {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$ciao'):
+        await message.channel.send(f'Ciao! Io sono un bot{client.user}!')
+    elif  message.content.startswith('$heh'):
+        if len(message.content) > 4:
+            count_heh = int(message.content[4:])
+        else:
+            count_heh = 5
+        await message.channel.send("he" * count_heh)
+        
+client.run("TOKEN")
